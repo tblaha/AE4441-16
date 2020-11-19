3e46c60d94278d0366e5eca0bccd9feb1a6ebbab
Till Blaha <till.blaha@web.de>
2020-11-19 14:53:53 +0100

    Cartopy Visualization prototype done; integration with simulation output next

--- Lib/plotting.py
+++ Lib/plotting.py
@@ -6,9 +6,6 @@ Created on Wed Nov 18 21:19:54 2020
 @author: tblaha
 """
 
-
-
-
 #% reset python workspace
 from IPython import get_ipython
 get_ipython().magic('reset -sf')
@@ -28,11 +25,11 @@ from cartopy.io.img_tiles import OSM
 
 
 class netgif():
-    def __init__(self):
+    def __init__(self, extend, zoom):
         
         # characteristics
-        self.extent = [14.65, 15.2, 54.97, 55.31]
-        self.zoomlevel = 10
+        self.extent = extend
+        self.zoomlevel = zoom
         self.figsize = [12, 9]
         
         # make basic figure size
@@ -46,33 +43,47 @@ class netgif():
         
     
     
-    def plot_series(self, grid, grid_conn, outpath, fnamebase):
+    def plot_series(self, grid, grid_conn, EVs, outpath, fnamebase):
         self.grid = grid
         
+        
+        # colormapping
+        self.cmap = plt.cm.jet
+        self.norm = mpl.colors.Normalize(vmin=0, vmax=110)
+        
+        # add grid
         self._add_grid()
         
-        
+        # iterate over discrete times k
         k_array = np.unique(grid_conn["time"])
-        with io.get_writer(outpath+"/"+fnamebase+".gif", 
+        with io.get_writer(outpath+"/"+fnamebase+".gif", # this sets up the gif
                            mode='I', 
                            duration=1.5
                            ) as writer:
             
             for k in k_array:
+                
                 # add the meat of the plot
                 self._add_links(grid_conn.loc[grid_conn["time"] == k, :])
+                self._add_cars(EVs.loc[EVs["time"] == k, :])
                 
-                filename = outpath + "/" + fnamebase + '_' + str(k) + '.eps'
+                # make colorbar
+                self.cb = plt.colorbar(self.grid_quivers)
+                self.cb.set_label("% Load")
+                
+                # save file as png graphics
+                filename = outpath + "/" + fnamebase + '_' + str(k) + '.png'
                 plt.savefig(filename, 
-                            format='eps', 
+                            format='png', 
                             )
                 
-                writer.append_data(io.imread(filename, format='eps'))
+                # open again as image and append it to the gif
+                writer.append_data(io.imread(filename, format='png'))
                 
-                
-                # clean the plot up so that only the OSM and the grid nodes remain
+                # clean the plot up so that only the OSM and the grid nodes 
+                # remain
                 self._clean_plot()
-            
+        
         
             
     def _add_grid(self):
@@ -96,51 +107,92 @@ class netgif():
         
         grid = self.grid
         
-        x, y = grid.loc[grid_conn["conn1"], ["Long", "Lat"]].to_numpy().T
-        xpu, ypv = grid.loc[grid_conn["conn2"], ["Long", "Lat"]].to_numpy().T
+        x1, y1 = grid.loc[grid_conn["conn1"], ["Long", "Lat"]].to_numpy().T
+        x2, y2 = grid.loc[grid_conn["conn2"], ["Long", "Lat"]].to_numpy().T
+        cap = grid_conn["Cap"].to_numpy()
+        load = grid_conn["Load"].to_numpy()
         
-        
-        idx_flip = 1-(grid_conn["Load"] >= 0).to_numpy()
-        
-        for i, b in enumerate(idx_flip):
-            if b:
-                (x[i], y[i], xpu[i], ypv[i]) = (xpu[i], ypv[i], x[i], y[i])
-        
-        u = xpu - x
-        v = ypv - y
-        
-        phi = np.arctan2(v, u)
-        sf = 1 + 0.78*(np.sin(phi))**(1.8)
-        
-        
-        pload = 100 * (np.abs(grid_conn["Load"]) / grid_conn["Cap"]).to_numpy()
-        
-        
-        # colorbar
-        self.cmap = plt.cm.jet
-        self.norm = mpl.colors.Normalize(vmin=0, vmax=max(110, np.max(pload)))
+        x, y, u, v, pload = self._process_arrows(x1, y1, x2, y2, cap, load)
         
         self.grid_quivers = self.ax.quiver(x,
                                            y,
-                                           u*sf,
-                                           v*sf,
+                                           u,
+                                           v,
                                            pload,
                                            angles='xy',
                                            scale_units='xy',
                                            scale=8.98*1e-6,
+                                           width=0.005,
                                            linewidth=1,
                                            transform=ccrs.PlateCarree(), 
                                            cmap=self.cmap,
                                            norm=self.norm,
                                            )
-        self.cb = plt.colorbar(self.grid_quivers)
-        self.cb.set_label("% Load")
+        
+        
+    def _process_arrows(self, x1, y1, x2, y2, cap, load):
+        
+        idx_flip = 1 - (load >= 0).astype(int)
+        
+        # flip destination/source of arrow based on sign of the load 
+        for i, b in enumerate(idx_flip):
+            if b:
+                (x1[i], y1[i], x2[i], y2[i]) = (x2[i], y2[i], x1[i], y1[i])
+        
+        # vector coordinates
+        u = x2 - x1
+        v = y2 - y1
+        
+        # scale vectors according to some reverse engineering of the shitty 
+        # cartopy interface
+        phi = np.arctan2(v, u)
+        sf = 1 + 0.78*(np.sin(abs(phi)))**(1.8) # scaling factor
+        (u, v) = (u * sf, v * sf) 
+        
+        # percentage load of the link for the colormap:
+        pload = 100 * (np.abs(load) / cap)
+        
+        return x1, y1, u, v, pload
+    
+    
+        
+    def _add_cars(self, EV):
+        
+        grid = self.grid
+        
+        x1, y1 = grid.loc[EV["grid"], ["Long", "Lat"]].to_numpy().T
+        x2, y2 = EV[["Long", "Lat"]].to_numpy().T
+        cap = EV["Cap"].to_numpy()
+        load = EV["Load"].to_numpy()
+        
+        x, y, u, v, pload = self._process_arrows(x1, y1, x2, y2, cap, load)
+        
+        self.car_quivers  = self.ax.quiver(x,
+                                           y,
+                                           u,
+                                           v,
+                                           pload,
+                                           angles='xy',
+                                           scale_units='xy',
+                                           scale=8.98*1e-6,
+                                           linestyle="--",
+                                           edgecolor="k",
+                                           facecolor='none',
+                                           linewidth=1,
+                                           width=0.002,
+                                           headwidth=10,
+                                           headlength=14,
+                                           transform=ccrs.PlateCarree(), 
+                                           cmap=self.cmap,
+                                           norm=self.norm,
+                                           )
         
         
         
     def _clean_plot(self):
         self.cb.remove()
         self.grid_quivers.remove()
+        self.car_quivers.remove()
 
         
 
--- viz.py
+++ viz.py
@@ -1,6 +1,8 @@
-import numpy as np 
+import numpy as np
+
 import pandas as pd
 from Lib import plotting as pt
+import matplotlib.pyplot as plt 
 
 grid = pd.DataFrame(columns=["Long", "Lat", "Type", "Color", "Size"])
 grid = grid.astype({"Long": float, 
@@ -13,7 +15,12 @@ grid.loc["nodeA"] = [15, 55.1, "60kV Station", "Orange", 50]
 grid.loc["nodeB"] = [14.7, 55.2, "60kV Station", "Orange", 50]
 grid.loc["nodeC"] = [15.1, 55.2, "60kV Station", "Orange", 50]
 
-grid_conn = pd.DataFrame(columns=["conn1", "conn2", "Cap", "time", "Load"])
+grid_conn = pd.DataFrame(columns=["conn1",
+                                  "conn2", 
+                                  "Cap",
+                                  "time",
+                                  "Load"]
+                         )
 grid_conn = grid_conn.astype({"conn1": str, 
                               "conn2": str, 
                               "Cap": float, 
@@ -32,11 +39,47 @@ grid_conn.loc[8] = ["nodeA", "nodeC", 100, 2, 97]
 grid_conn.loc[9] = ["nodeB", "nodeC", 100, 2, -98]
 
 
+EV = pd.DataFrame(columns=["Long", 
+                           "Lat", 
+                           "Type", 
+                           "Color", 
+                           "Size", 
+                           "Cap",
+                           "time", 
+                           "grid", 
+                           "Load", 
+                           ])
+EV = EV.astype({"Long": float, 
+                "Lat": float,
+                "Type": str,
+                "Color": str,
+                "Size": float,
+                "Cap": float,
+                "time": int,
+                "grid": str,
+                "Load": float,
+                })
+
+EV.loc["EV_1_0"] = [15.10, 55.15, "Car", "Blue", 30, 11, 0, "nodeA", 5]
+EV.loc["EV_2_0"] = [14.90, 55.22, "Car", "Blue", 30, 11, 0, "nodeA", 0]
+EV.loc["EV_3_0"] = [14.75, 55.10, "Car", "Blue", 30, 11, 0, "nodeB", -11]
+EV.loc["EV_1_1"] = [15.10, 55.15, "Car", "Blue", 30, 11, 1, "nodeA", -5]
+EV.loc["EV_2_1"] = [14.90, 55.22, "Car", "Blue", 30, 11, 1, "nodeA", 0]
+EV.loc["EV_3_1"] = [14.75, 55.10, "Car", "Blue", 30, 11, 1, "nodeB", 11]
+EV.loc["EV_1_2"] = [15.10, 55.15, "Car", "Blue", 30, 11, 2, "nodeA", 0]
+EV.loc["EV_2_2"] = [14.90, 55.22, "Car", "Blue", 30, 11, 2, "nodeA", 5]
+EV.loc["EV_3_2"] = [14.75, 55.10, "Car", "Blue", 30, 11, 2, "nodeB", 9]
+
+
+
 outpath = "./plots/"
 fnamebase = "test"
 
-c=pt.netgif()
-c.plot_series(grid, grid_conn, outpath, fnamebase)
+c=pt.netgif([14.65, 15.2, 54.97, 55.31], 10)
+# c=pt.netgif([14.65, 14.75, 55.175, 55.225], 13)
+c.plot_series(grid, grid_conn, EV, outpath, fnamebase)
+
+plt.close("all")
 
 
 
@@ -45,10 +88,10 @@ c.plot_series(grid, grid_conn, outpath, fnamebase)
 #     longEV, latEV = row[["Long", "Lat"]].to_numpy()
 #     longGrid, latGrid = grid.loc[row["grid"], ["Long", "Lat"]].to_numpy()
 #     plt.plot(np.array([longEV, longGrid]),
-#              np.array([latEV, latGrid]),
-#              transform=ccrs.PlateCarree(),
-#              marker="", color="blue"
-#              )
+#               np.array([latEV, latGrid]),
+#               transform=ccrs.PlateCarree(),
+#               marker="", color="blue"
+#               )
     
 # plt.plot(EV["Long"].to_numpy(), EV["Lat"].to_numpy(), 
 #          transform=ccrs.PlateCarree(), marker="o", color="Blue", 
